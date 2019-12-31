4bbe7a737c2174fc2fadffb4613b7f7aaf3cc403
crispinamuriel <crispinamuriel@gmail.com>
2019-12-31 17:25:27 -0500

    style: finishing touches to aboutme

--- client/Contact.js
+++ client/Contact.js
@@ -3,34 +3,49 @@ const Contact = () => (
   <div id="stories" className="column">
     <div className="story3 opacity contact-link">
       <hr />
-      <h3>Email:</h3>
       <a
         className="padding contact-link"
         href="mailto:crispinamuriel@gmail.com"
       >
-        crispinamuriel@gmail.com
+        <div className="centered">
+          <img src="gmail.png" width="50" />
+        </div>
+        <div className="margin-left">
+          <h3>Email:</h3>
+          crispinamuriel@gmail.com
+        </div>
       </a>
       <hr />
     </div>
     <div className="story3 opacity">
       <hr />
-      <h3>LinkedIn:</h3>
       <a
         className="padding contact-link"
         href="https://www.linkedin.com/in/crispinarmuriel/"
       >
-        https://www.linkedin.com/in/crispinarmuriel/
+        <div className="centered">
+          <img src="linkedin.png" width="50" />
+        </div>
+        <div className="margin-left">
+          <h3>LinkedIn:</h3>
+          https://www.linkedin.com/in/crispinarmuriel/
+        </div>
       </a>
       <hr />
     </div>
     <div className="story3 opacity">
       <hr />
-      <h3>Github:</h3>
       <a
         className="padding contact-link"
         href="https://github.com/crispinamuriel"
       >
+        <div className="centered">
+          <img src="github.png" width="50" />
+        </div>
+        <div className="margin-left">
+          <h3>Github:</h3>
         https://github.com/crispinamuriel
+        </div>
       </a>
       <hr />
     </div>
